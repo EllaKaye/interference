63a33eb09eb54681ee4f0d3a0828277d7bdc3094
EllaKaye <ella.kaye@gmail.com>
2024-09-04 08:10:14 +0100

    Add modal and input reactiveness back in

--- server.py
+++ server.py
@@ -23,6 +23,57 @@ def server(input, output, session):
         game_state.set(game_state() + 1)
 
     @reactive.effect
+    @reactive.event(input.new_game_modal)
+    def _():
+        ui.modal_remove()
+        game_instance = game()
+        game_instance.new_game()
+        game.set(game_instance)
+        dragged_card.set(None)
+        game_info_message.set("New game started")
+        debug_message.set("New game started")
+        game_state.set(game_state() + 1)
+
+    @reactive.effect
+    @reactive.event(input.new_round)
+    def _():
+        ui.modal_remove()
+        game_instance = game()
+        result = game_instance.new_round()
+        game.set(game_instance)
+        game_info_message.set("New round started")
+        debug_message.set(result)
+        game_state.set(game_state() + 1)
+
+    @reactive.effect
+    @reactive.event(input.new_round_modal)
+    def _():
+        ui.modal_remove()
+        game_instance = game()
+        result = game_instance.new_round()
+        game.set(game_instance)
+        game_info_message.set("New round started")
+        debug_message.set(result)
+        game_state.set(game_state() + 1)
+    
+    @reactive.effect
+    def _():
+        game_over_title = game().game_over_title()
+        if game_over_title:
+            ui.modal_show(
+                game_over_modal(game_over_title)
+            )
+
+    @reactive.effect
+    def _():
+        round_over_title = game().round_over_title()
+        if round_over_title:
+            ui.modal_show(
+                round_over_modal(round_over_title)
+            )
+
+
+    @reactive.effect
     @reactive.event(input.swap_cards)
     def _():
         if input.swap_cards():
